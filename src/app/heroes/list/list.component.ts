ef16f5b54a710c3a3a0122386e151f43bc472bc7
DesarrolladorAndresCalero <desarrollador.andrescalero@gmail.com>
2024-02-01 20:36:18 -0500

    aprendiendo bases de angular

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+  public heroNames: string[] = ['Spiderman', 'IronMan', 'Hulk', 'Thor'];
+  public deleteHero?:string;
+
+  removeLastHeroe():void{
+    this.deleteHero = this.heroNames.pop();
+  }
+}
